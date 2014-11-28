1c74bec9405105a74e86d3d52c9d4644cba62e57
Jeff Ong <jonger4@gmail.com>
2014-11-28 15:01:53 -0500

    working zoom alters sequence

--- stockMusic/index.js
+++ stockMusic/index.js
@@ -3,6 +3,35 @@
 var comparisonChart = require('comparisonChart');
 var $ = require('jquery');
 var d3 = require('d3');
+var ge = require('ge');
+var aapl = require('aapl');
+var nflx = require('nflx');
+var Music = require('music');
+var _ = require('underscore');
+
+function clean(d){
+	var cleaned = [];
+
+	var dates = d.Dates;
+		dates = dates.slice(Math.max(dates.length - 90));
+
+	var prices = d.Elements[0].DataSeries.close.values;
+	prices = prices.slice(Math.max(prices.length - 90));
+
+	for (var i = 0; i < dates.length; i++){
+		var d = dates[i].replace('T','-');
+			d = d.split('-');
+
+		var datePrice = {
+			date: new Date( parseInt(d[0]), parseInt(d[1]-1), parseInt(d[2]) ),
+			close: prices[i]
+		};
+
+		cleaned.push(datePrice);
+	}
+
+	return cleaned;
+}
 
 var Stock = function(tickerSymbol) {
 	this.ticker = tickerSymbol.toUpperCase();
@@ -34,6 +63,9 @@ Stock.prototype.init = function() {
 	})
 	.done(function(d) {
 		// console.log("success");
+
+		console.log(JSON.stringify(d));
+
 		var dates = d.Dates;
 		dates = dates.slice(Math.max(dates.length - 90));
 
@@ -49,7 +81,7 @@ Stock.prototype.init = function() {
 				d = d.split('-');
 
 			var datePrice = {
-				date: new Date( parseInt(d[0]), parseInt(d[1]), parseInt(d[2]) ),
+				date: new Date( parseInt(d[0]), parseInt(d[1]-1), parseInt(d[2]) ),
 				close: prices[i]
 			};
 
@@ -63,15 +95,7 @@ Stock.prototype.init = function() {
 			}
 		});
 
-		console.log(data);
-
-		comparisonChart("Geometric", data);
-		
-		// $('#stocklist').append('<li class="list-group-item">'+self.ticker+'</li>');
-
-		// if(stocks.length === 1){
-		// 	callback(stocks, synths);					
-		// }
+		// comparisonChart("Geometric", data);
 
 	})
 	.fail(function() {
@@ -82,6 +106,103 @@ Stock.prototype.init = function() {
 	});
 };
 
-var ge = new Stock('ge');
+Stock.prototype.init_Steps = function(prices, deltas) {
+	var referenceFreq = 49;
+
+	var min = _.min(prices);
+	var max = _.max(prices);
+
+	this.steps = [];
+	this.music = new Music(referenceFreq);
+	this.music 
 	
-	ge.init();
+	for(var note = 0; note < prices.length; note++){
+		this.steps[note] = this.music.snapToNote( ( prices[note] ), min, max );
+	}
+
+	// console.log(this.steps);
+}
+
+Stock.prototype.init_Synth = function() {
+	var presets = [ "Trumpet", "Koto", "Barky", "Pianoetta", "LaserStep"];
+
+	this.synth = new Tone.MonoSynth();
+	this.synth.setPreset("Koto");
+	this.synth.oscillator.setType('sine');
+	this.synth.setVolume(-20);
+	this.synth.toMaster();
+
+	// this.synth = new Tone.MonoSynth();
+	// this.synth.setPreset(presets[Math.round(Math.random()*5)]);
+	// this.synth.oscillator.setType();
+	// this.synth.setVolume(-20);
+	// this.synth.toMaster();
+}
+
+////////////////////////////////////////////////////////////////////////////////
+//																			  //
+//							  THE APP 										  //
+//																			  //
+////////////////////////////////////////////////////////////////////////////////
+
+ge = clean(ge);
+aapl = clean(aapl);
+nflx = clean(nflx);
+
+var stocknames = [ 'ge', 'aapl', 'nflx' ];
+
+var stockdata = [];
+
+stockdata.push(ge, aapl, nflx);
+
+var data = [];
+var stocks = [];
+
+for (var i = 0; i < stocknames.length; i++){
+	var stockobject = {
+		name: stocknames[i],
+		data: stockdata[i]
+	}
+
+	data.push(stockobject);
+
+	var prices = [];
+	var percentage_change = [];
+
+	for(var s = 0; s < stockdata[i].length; s++){
+		prices.push(stockdata[i][s].close);
+
+		percentage_change.push(stockdata[i][s] / stockdata[i][0]);
+	}
+
+	var stock = new Stock(stocknames[i]);
+
+	stock.data = stockobject;
+
+	stock.init_Steps(prices, percentage_change);
+	stock.init_Synth();
+	
+	stocks.push(stock);
+}
+
+var stepNum = 0;
+var stepArray = [0,1,2,3,4,5,6,7];
+
+Tone.Transport.loop = true;
+Tone.Transport.setBpm( 144 );
+
+console.log(data);
+
+var chart = comparisonChart("Geometric", data, Tone.Transport, stocks, stepArray);
+
+Tone.Transport.setInterval(function(time){
+	stepNum++;
+	stepNum = stepNum % 8;
+	
+	for(var s = 0; s < stocks.length; s++){
+		stocks[s].synth.triggerAttackRelease(stocks[s].steps[stepArray[stepNum]], '8n');
+	}
+
+}, "8n");
+
+Tone.Transport.start();
